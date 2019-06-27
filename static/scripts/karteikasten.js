58bf5f3aabbc5f12b3b89110f10c85b6cdb198c7
Moritz Momper <moritz.momper@th-koeln.de>
2019-06-27 16:04:47 +0200

    neuladen bei gleicher url des filters verhindert

--- static/scripts/karteikasten.js
+++ static/scripts/karteikasten.js
@@ -1,22 +1,34 @@
+var url;
 $(document).ready(function () {
     $('#karteikasten-filter-kategorie').click(onChange);
 })
 
 function onChange() {
-    let selected = $(':selected', $(this))
-    unterkategorie = selected.attr("value")
-    if (unterkategorie === "") {
-        window.location.href = window.location.href.split('?')[0]
-    } else {
-        oberkategorie = selected.parent().attr("label")
 
-        path = window.location.href.split('?')[0]
-        if (path.charAt(path.length - 1) == '#') {
-            path = path.substring(0, path.length - 1);
+    let selected = $(':selected', $(this))
+    let unterkategorie = selected.attr("value");
+    if (unterkategorie === "") {
+        let tmp = window.location.href;
+
+        if (tmp != tmp.split('?')[0]) {
+            window.location.href = tmp.split('?')[0];
+        }
+    } else {
+        let tmp = window.location.href;
+
+        let oberkategorie = selected.parent().attr("label")
+
+        let url = window.location.href.split('?')[0];
+        if (url.charAt(url.length - 1) == '#') {
+            url = url.substring(0, url.length - 1);
         }
 
-        window.location.href = path
+        url = url
             + "?" + "oberkategorie" + "=" + oberkategorie
             + "&" + "unterkategorie" + "=" + unterkategorie;
+
+        if (url != tmp) {
+            window.location.href = url;
+        }
     }
 }
